f107197eb71d88bd394aeaf50eca4ff27c709bba
lodger-c64 <nhaedecke@timocom.com>
2015-07-09 13:26:01 +0200

    updated .d64 image

--- platform/c64/contiki-bbs.c
+++ platform/c64/contiki-bbs.c
@@ -52,7 +52,7 @@
 #include "telnetd.h"
 #include "shell.h"
 
-char telnetd_reject_text[] = "Contiki BBS is busy, please try again later.\n";
+char telnetd_reject_text[] = "Contiki BBS is busy, please try again later.";
 
 /*---------------------------------------------------------------------------*/
 PROCESS(shell_init_process, "Shell init process");
